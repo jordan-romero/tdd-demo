2f03edb5da18fb898fabebc8781dc38cbdee33c5
jordles113 <jordan.t.romero@gmail.com>
2021-01-01 17:06:31 -0600

    installs enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+
+import Enzyme from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
 import App from './App';
 
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
 });
