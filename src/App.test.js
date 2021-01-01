66ee7786f5ac1d6ee6d05dabca0b516ad2aab809
jordles113 <jordan.t.romero@gmail.com>
2021-01-01 17:27:10 -0600

    makes the first test!

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 
-import Enzyme from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 
 import App from './App';
@@ -7,5 +7,7 @@ import App from './App';
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders learn react link', () => {
-  
+  const wrapper = shallow(<App />)
+  expect(wrapper).toBeTruthy()
+  // console.log(wrapper.debug()) -- prints the whole dom in the console as a string .debug() does this
 });
